01dd07705ed9299187770e6dbe9dfb02ce9dd8da
ujjalsharma <ujjalsharma81@gmail.com>
2020-07-04 18:16:34 +0530

    account info updated

--- app/src/main/java/com/confessionapp/UpdateAccoutInfoActivity.kt
+++ app/src/main/java/com/confessionapp/UpdateAccoutInfoActivity.kt
@@ -3,47 +3,38 @@ package com.confessionapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
-import com.google.android.material.navigation.NavigationView
+import android.widget.EditText
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
-import kotlinx.android.synthetic.main.activity_account_info.*
 
-class AccountInfoActivity : AppCompatActivity() {
+class UpdateAccoutInfoActivity : AppCompatActivity() {
+
     val mAuth = FirebaseAuth.getInstance()
-    var accountEmailTextView: TextView? = null
-    var accountNameTextView: TextView? = null
+    var accountNameEditText: EditText? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_account_info)
+        setContentView(R.layout.activity_update_accout_info)
 
 
-        //Set up Account Information
-        accountEmailTextView = findViewById(R.id.accountEmailTextView)
-        accountEmailTextView?.text = mAuth.currentUser?.email.toString()
-        accountNameTextView = findViewById(R.id.accountNameTextView)
+        accountNameEditText = findViewById(R.id.accountNameEditText)
         FirebaseDatabase.getInstance().getReference().child("users").child(mAuth.currentUser?.uid.toString()).child("name").addListenerForSingleValueEvent(object :
             ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                accountNameTextView?.text = snapshot.value.toString()
+                accountNameEditText?.setText(snapshot.value.toString())
             }
             override fun onCancelled(error: DatabaseError) { }
         })
     }
 
     override fun onBackPressed() {
-        val intent = Intent(this, MainFeedActivity::class.java)
+        val intent = Intent(this, AccountInfoActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
-        startActivity(intent)
-    }
-
-    fun updateInfoClicked(view: View){
-        val intent = Intent(this, UpdateAccoutInfoActivity::class.java)
         startActivity(intent)
     }
 }
